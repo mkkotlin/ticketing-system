b3639f1476083c340221732020ff037f7e30b2e8
mayank <123.ultron.1234@gmail.com>
2026-08-30 09:54:11 +0530

    feat: configure SQLAlchemy URL in Alembic and add pyproject.toml for project dependencies

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -12,6 +12,9 @@ from app import models
 # access to the values within the .ini file in use.
 config = context.config
 
+from app.config import settings
+config.set_main_option("sqlalchemy.url", settings.database_url)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
